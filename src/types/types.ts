d838fcff1345b7ae765d1f654b8809fc8e3ff22a
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-02 13:36:48 +0300

    удалил лишние определения типов

--- src/types/types.ts
+++ src/types/types.ts
@@ -35,37 +35,3 @@ export type TodoListActions = {
   edit: () => void
   remove: () => void
 }
-
-export type ButtonPrimaryColors = {
-  [index: string]: string
-
-  red: '#EB5757'
-  green: '#27AE60'
-  blue: '#3C4FF4'
-  white: 'white'
-  transparent: 'transparent'
-}
-
-export type ButtonTextColors = {
-  [index: string]: string
-
-  white: 'white'
-  black: 'black'
-}
-
-export type ButtonTheme = {
-  background: keyof ButtonPrimaryColors
-  text: keyof ButtonTextColors
-}
-
-export type ButtonStyleVariants = 'primary' | 'danger'
-
-export type ButtonStyles = {
-  [index in ButtonStyleVariants]: ButtonTheme
-}
-
-export type AppTheme = {
-  button: ButtonStyles
-}
-
-export type HTMLButtonType = 'button' | 'submit' | 'reset'
